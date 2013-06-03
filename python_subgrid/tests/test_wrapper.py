e7f4d6f59b7e9f87c4433db026cdddb0656e910c
reinout <reinout@vanrees.org>
2013-06-03 13:05:40 +0000

    Improved test coverage

--- python_subgrid/tests/test_wrapper.py
+++ python_subgrid/tests/test_wrapper.py
@@ -1,9 +1,20 @@
 import unittest
 
+import mock
+
 from python_subgrid import wrapper
 
 
 class TestHelperFunctions(unittest.TestCase):
 
     def test_libname1(self):
-        self.assertEquals(wrapper._libname(), 'libsubgrid.so')
+        with mock.patch('platform.system', lambda: 'Linux'):
+            self.assertEquals(wrapper._libname(), 'libsubgrid.so')
+
+    def test_libname2(self):
+        with mock.patch('platform.system', lambda: 'Darwin'):
+            self.assertEquals(wrapper._libname(), 'libsubgrid.dylib')
+
+    def test_libname3(self):
+        with mock.patch('platform.system', lambda: 'Windows'):
+            self.assertEquals(wrapper._libname(), 'libsubgrid.dll')
