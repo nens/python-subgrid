8692cf9c352487e38556026ebc5ebef832eb86ee
reinout <reinout@vanrees.org>
2013-07-10 14:58:34 +0000

    Test setup tweaks

--- python_subgrid/tests/test_utils.py
+++ python_subgrid/tests/test_utils.py
@@ -1,10 +1,12 @@
+import os
 import unittest
 
 import mock
 
 from python_subgrid import utils
 
-
+@unittest.skipIf(not os.path.exists('doc/source/index.rst'), 
+                 'doc/source dir not available')
 class TestUtils(unittest.TestCase):
 
     def test_generate_functions_documentation(self):
