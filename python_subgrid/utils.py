96bb03a151a1e435cb3fe5097f62e9c9883f7a3b
Fedor Baart <fedor.baart@deltares.nl>
2014-01-09 16:56:12 +0100

    accidently removed multisection writer

--- python_subgrid/utils.py
+++ python_subgrid/utils.py
@@ -154,7 +154,22 @@ class MultiSectionConfigParser(configparser.ConfigParser):
 
         self._sections.add(section, self._dict())
 
-
+    def write(self, fp):
+        """Write an .ini-format representation of the configuration state."""
+        if self._defaults:
+            fp.write("[%s]\n" % configparser.DEFAULTSECT)
+            for (key, value) in self._defaults.items():
+                fp.write("%s = %s\n" % (key, str(value).replace('\n', '\n\t')))
+            fp.write("\n")
+        for section, options in self._sections.items():
+            fp.write("[%s]\n" % section)
+            for (key, value) in options.items():
+                if key == "__name__":
+                    continue
+                if (value is not None) or (self._optcre == self.OPTCRE):
+                    key = " = ".join((key, str(value).replace('\n', '\n\t')))
+                fp.write("%s\n" % (key))
+            fp.write("\n")
 
 class NotDocumentedError(Exception):
     pass
