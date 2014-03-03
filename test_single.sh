a3523b7b979667150192d3625cfcd96b98172be2
Fedor Baart <fedor.baart@deltares.nl>
2014-03-03 14:07:58 +0100

    example added

--- test_single.sh
+++ test_single.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 bin/test python_subgrid/tests/test_functional.py:LibSubgridTest.$1 $2 $3
+# for debugging do:
+# gdb --args python bin/test python_subgrid/tests/test_functional.py:LibSubgridTest.$1 $2 $3
 
