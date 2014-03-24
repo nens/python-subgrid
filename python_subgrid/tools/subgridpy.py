02609d98f4a91524174a6e1d9f263f2137574ca7
Fedor Baart <fedor.baart@deltares.nl>
2014-03-24 13:54:35 +0100

    cleanup scripts, add runner script

---
+++ python_subgrid/tools/subgridpy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Run subgrid as a python script with colored output
+"""
+import argparse
+import sys
+
+
+from python_subgrid.wrapper import SubgridWrapper, logger, progresslogger, NotDocumentedError
+from python_subgrid.tests.utils import colorlogs
+colorlogs()
+
+try:
+    # redirect stdout to /dev/null under osx so we get only 1 output stream
+    f = open('/dev/null', 'w')
+    sys.stdout = f
+except:
+    pass
+
+def parse_args():
+    """
+    Parse the command line arguments
+    """
+    argumentparser = argparse.ArgumentParser(
+        description='Run subgrid')
+    argumentparser.add_argument('mdu', help='mdu files to process')
+    arguments = argumentparser.parse_args()
+    return arguments
+
+def main():
+    """main program"""
+    arguments = parse_args()
+
+    # Read mdu file
+    with SubgridWrapper(mdu=arguments.mdu) as subgrid:
+        t_end = subgrid.get_nd('tend')
+        t = subgrid.get_nd('t1')
+        while t < t_end:
+            subgrid.update(-1)
