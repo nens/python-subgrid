92efd9986fdb7579bac7b1c572d5efda76954ceb
Fedor Baart <fedor.baart@deltares.nl>
2014-05-14 16:33:02 +0200

    fix rename in shell script

--- valgrind_single.sh
+++ valgrind_single.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-valgrind --track-origins=yes --gen-suppressions=all --suppressions=extra.supp --suppressions=valgrind-python.supp  python bin/test python_subgrid/tests/test_functional.py:LibSubgridTest.$1 $2 $3
+valgrind --track-origins=yes --gen-suppressions=all --suppressions=extra.supp --suppressions=valgrind-python.supp  python bin/test python_subgrid/tests/test_functional.py:TestCase.$1 $2 $3
 
