ca8d3cf3ffc72a26455288e7abfc066673f6772c
Arjan Verkerk <arjan.verkerk@gmail.com>
2015-06-18 10:44:49 +0200

    Raingrid now fetches rain from raster-server and interpolates not anymore.

--- python_subgrid/raingrid.py
+++ python_subgrid/raingrid.py
@@ -1,13 +1,20 @@
-import os
-import numpy as np
-import netCDF4
+# -*- coding: utf-8 -*-
+from __future__ import division
+
 import datetime
 import logging
-import scipy.interpolate
+import os
+import urllib
+import urlparse
+
+from osgeo import gdal
+import netCDF4
+import numpy as np
 
 
 logger = logging.getLogger(__name__)
 
+POLYGON = 'POLYGON (({x1} {y1},{x2} {y1},{x2} {y2},{x1} {y2},{x1} {y1}))'
 
 AREA_WIDE_RAIN = {
     '0': [0.0],
@@ -25,15 +32,51 @@ AREA_WIDE_RAIN = {
     }
 
 
+def get_rain(bbox, width, height, datetime,
+             srs='epsg:28992',
+             layer='radar:5min',
+             server='https://raster.lizard.net'):
+        """
+        Fetches rain from raster server.
+        """
+        x1, x2, y1, y2 = bbox
+
+        parameters = {'sr': srs,
+                      'width': width,
+                      'height': height,
+                      'layers': layer,
+                      'compress': 'deflate',
+                      'request': 'getgeotiff',
+                      'time': datetime.isoformat(),
+                      'geom': POLYGON.format(x1=x1, y1=y1, x2=x2, y2=y2)}
+
+        url = '{path}?{pars}'.format(pars=urllib.urlencode(parameters),
+                                     path=urlparse.urljoin(server, 'data'))
+        logger.info('Loading rain data from %s...' % url)
+
+        # receive tif into gdal vsimem
+        vsi_path = '/vsimem/rain.tif'
+        vsi_file = gdal.VSIFOpenL(str(vsi_path), b'w')
+        url_file = urllib.urlopen(url)
+        size = int(url_file.info().get('content-length'))
+        gdal.VSIFWriteL(url_file.read(), size, 1, vsi_file)
+        gdal.VSIFCloseL(vsi_file)
+
+        # fetch array and free vsimem storage
+        rain = gdal.Open(vsi_path).ReadAsArray()
+        gdal.Unlink(vsi_path)
+        return rain
+
+
 class RainGrid(object):
     """
     Manage a rain grid.
 
     Only works on the Netherlands.
     """
-    def __init__(self, subgrid, url_template='',
+    def __init__(self, subgrid, url_template=None,
                  memcdf_name='precipitation.nc',
-                 size_x=500, size_y=500, initial_value=0.0):
+                 size_x=None, size_y=None, initial_value=0.0):
         """subgrid is used to initialize the rain grid.
 
         url_template is needed in function update: it fetches data from an
@@ -41,69 +84,55 @@ class RainGrid(object):
         """
         if not url_template:
             logger.warning('No url_template given.')
-        self.url_template = url_template
-        self.size_x = size_x
-        self.size_y = size_y
+        if size_x:
+            logger.warning('Ignoring deprecated keyword argument: size_x')
+        if size_y:
+            logger.warning('Ignoring deprecated keyword argument: size_y')
+
         self.dt_current = None
         self.memcdf_name = memcdf_name
         self.diskless = False
 
         # Read pixels in model to create grid for rain
-        pixels = {}
-        pixels['dps'] = subgrid.get_nd('dps')[1:-1, 1:-1].T
-        # ^^^ contains ghost cells? # why transposed
-        pixels['dps'] = np.ma.masked_array(
-            -pixels['dps'], mask=pixels['dps'] == pixels['dps'].min())
-        pixels['x0p'] = subgrid.get_nd('x0p')
-        pixels['dxp'] = subgrid.get_nd('dxp')
-        pixels['y0p'] = subgrid.get_nd('y0p')
-        pixels['dyp'] = subgrid.get_nd('dyp')
-        pixels['imax'] = subgrid.get_nd('imax')
-        pixels['jmax'] = subgrid.get_nd('jmax')
-        pixels['x'] = np.arange(
-            pixels['x0p'],
-            pixels['x0p'] + pixels['imax'] * pixels['dxp'],
-            pixels['dxp'])
-        pixels['y'] = np.arange(
-            pixels['y0p'],
-            pixels['y0p'] + pixels['jmax'] * pixels['dyp'],
-            pixels['dyp'])
+        width = subgrid.get_nd('imax') + 1
+        height = subgrid.get_nd('jmax') + 1
 
-        # Create a new grid for our rain
-        self.interp = {}
-        self.interp['x'] = np.linspace(pixels['x'].min(),
-                                       pixels['x'].max(),
-                                       num=self.size_x)
-        self.interp['y'] = np.linspace(pixels['y'].min(),
-                                       pixels['y'].max(),
-                                       num=self.size_y)
-        self.interp['X'], self.interp['Y'] = np.meshgrid(self.interp['x'],
-                                                         self.interp['y'])
+        x1 = subgrid.get_nd('x0p')
+        y2 = subgrid.get_nd('y0p')
+
+        dx = subgrid.get_nd('dxp')
+        dy = -subgrid.get_nd('dyp')
+
+        x2 = x1 + width * dx
+        y1 = y2 - height * dy
+
+        self.bbox = x1, x2, y1, y2
+        self.width = width
+        self.height = height
 
         # TODO: replace precipitation.nc with a unique name
         # TODO: add diskless=True, requires netcdf version >= 4.2.x
         # For now use netcdf classic, issue with netcdf redefinition in hdf5
         # format
+        logger.info('Creating a %i x %i rain grid.', width, height)
         memcdf = netCDF4.Dataset(self.memcdf_name,
                                  mode="w",
                                  diskless=self.diskless,
                                  format='NETCDF3_64BIT')
 
-        memcdf.createDimension("nx", self.interp['x'].shape[0])
-        logger.info('interp x shape %r' % self.interp['x'].shape[0])
-        memcdf.createDimension("ny", self.interp['y'].shape[0])
-        logger.info('interp y shape %r' % self.interp['y'].shape[0])
+        memcdf.createDimension("nx", width)
+        memcdf.createDimension("ny", height)
 
         # Put coordinates and values in the netcdf
         var = memcdf.createVariable(
             "x", datatype="double", dimensions=("nx",))
-        var[:] = self.interp['x']
+        var[:] = np.linspace(x1 + dx / 2, x2 - dx / 2, width)
         var.standard_name = 'projected_x_coordinate'
         var.units = 'm'
 
         var = memcdf.createVariable(
             "y", datatype="double", dimensions=("ny", ))
-        var[:] = self.interp['y']
+        var[:] = np.linspace(y1 + dy / 2, y2 - dy / 2, height)
         var.standard_name = 'projected_y_coordinate'
         var.units = 'm'
 
@@ -124,75 +153,39 @@ class RainGrid(object):
             self.memcdf_name, mode="r+", diskless=self.diskless)
         rainfall_var = memcdf.variables["rainfall"]
         rainfall_var[:, :] = value
-        # interp['Z']*(1/5.0)*(1/1000.0) #  mm/5min * 5min/min * m/mm -> m/min
         memcdf.sync()
         memcdf.close()
 
     def update(self, dt, multiplier=1.0):
-        """Update the (interpolated) grid with given datetime
+        """Update the grid with rain at given datetime
 
         Return True if grid has changed"""
-        if not self.url_template:
-            logger.error('No url_template given, cannot use opendap server.')
-            return
-
         # Quantize on 5 minutes
-        minutes = dt.minute / 5 * 5
+        minutes = dt.minute // 5 * 5
         dt_request = datetime.datetime(dt.year, dt.month, dt.day, dt.hour,
                                        minutes, 0)
         if dt_request == self.dt_current:
             # Nothing to do
             return False
 
-        url = self.url_template.format(year=dt.year, month='%02d' % dt.month)
-        logger.info('Reading rain data from %s...' % url)
-        # url = 'http://opendap.nationaleregenradar.nl/thredds/dodsC/radar/TF0005_A/2013/10/01/RAD_TF0005_A_20131001000000.h5'
-        ds = netCDF4.Dataset(url)
-
-        rain = {}
-        rain['time'] = netCDF4.num2date(ds.variables['time'][:],
-                                        ds.variables['time'].units)
-        rain['x'] = ds.variables['east'][:]
-        rain['y'] = ds.variables['north'][:]
-        rain['P'] = ds.variables['precipitation']
-
-        # Pick the index of the requested datetime.
-        time_idx = np.where(rain['time'] == dt_request)[0]
-        rain['p'] = rain['P'][:, :, time_idx]
-        # ^^^ x, y expected by some routines
-
-        # Interpolate rain data rain -> interp
-        logger.info('Interpolating rain data...')
-        F = scipy.interpolate.RectBivariateSpline(
-            rain['x'].ravel(),
-            rain['y'][::-1],
-            # reverse y axis and swap x,y
-            np.swapaxes(rain['p'][::-1, :].filled(0), 0, 1)
-        )
-
-        # Evaluate the interpolation function on the new grid
-        self.interp['z'] = F.ev(self.interp['X'].ravel(),
-                                self.interp['Y'].ravel())
-
-        self.interp['Z'] = self.interp['z'].reshape(self.interp['X'].shape)
-
-        logger.info('Updating memcdf...')
-
-        ds.close()
+        rain = get_rain(bbox=self.bbox,
+                        width=self.width,
+                        height=self.height,
+                        datetime=dt_request)[::-1]
 
         memcdf = netCDF4.Dataset(self.memcdf_name,
                                  mode="a",
                                  diskless=self.diskless)
         rainfall_var = memcdf.variables["rainfall"]
-        rainfall_var[:, :] = (self.interp['Z'] * (1 / 5.0) * (1 / 1000.0) *
-                              multiplier)
-        # ^^^ mm/5min * 5min/min * m/mm -> m/min
+        rain /= 5     # to mm/min
+        rain /= 1000  # to m/min
+        rain *= multiplier
+
+        rainfall_var[:] = rain
         memcdf.sync()
         memcdf.close()
 
-        logger.info('Rainfall sum (testing): %f',
-                    np.sum(self.interp['Z'] * (1 / 5.0) * (1 / 1000.0) *
-                           multiplier))
+        logger.info('Rainfall maximum: %f', rain.max())
 
         self.dt_current = dt_request
         return True
@@ -223,8 +216,9 @@ class AreaWideRainGrid(RainGrid):
             else:
                 self.cumulative = 0
 
-        if ((new_value != self.current_value) or
-            (rain_definition != self.current_rain_definition)):
+        value_changed = new_value != self.current_value
+        definition_changed = rain_definition != self.current_rain_definition
+        if value_changed or definition_changed:
             logger.debug('New intensity area wide rain: time %ds new value %f',
                          time_seconds, new_value)
             # convert mm/300s to mm/min????
--- sandbox.py
+++ sandbox.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
-""" TODO Docstring. """
+"""
+Findings:
+- dps transpose questionable? Does it matter?
+- Range endpoint is exclusive.
+- Coordinates are now topleft coordinates instead of centers
+- Fetched data is of correct date and location
+- Spline sum is approximately same as neasest neighbour sum:
+    ('spline: ', 445.52081027555687)
+    ('nearest:', 452.0166)
+- Data is interpolated and stored with smallest y-coordinate first
+
+Changes:
+- Fix aranges
+- Remove interpolation alltogether
+- Data straight from raster-server as in-memory geotiff, see extract tool.
+
+Remaining questions:
+- Okay to interpolate exactly on dxp and dyp grid?
+- Okay to treat x0p and y0p as centers of cells?
+- Okay to return upside-down grid?
+"""
 
 from __future__ import print_function
 from __future__ import unicode_literals
@@ -34,20 +54,22 @@ class Subgrid(dict):
 
 
 def sandbox():
-    # subgrid = Subgrid('kockengen.tif')
-    # url_template = ('http://opendap.nationaleregenradar.nl'
-                    # '/thredds/dodsC/radar/TF0005_A/'
-                    # '{year}/{month}/01/RAD_TF0005_A_{year}{month}01000000.h5')
-    # rain_grid = RainGrid(subgrid=subgrid, url_template=url_template)
-    # rain_grid.update(Datetime(2014, 7, 28, 10))
+    subgrid = Subgrid('kockengen.tif')
+    url_template = ('http://opendap.nationaleregenradar.nl'
+                    '/thredds/dodsC/radar/TF0005_A/'
+                    '{year}/{month}/01/RAD_TF0005_A_{year}{month}01000000.h5')
+    rain_grid = RainGrid(subgrid=subgrid, url_template=url_template)
+
+    rain_grid.update(Datetime(2014, 7, 28, 10))
     logger.debug('done.')
 
 
 def get_parser():
     """ Return argument parser. """
-    parser = argparse.ArgumentParser(description=__doc__)
-    # add arguments here
-    # parser.add_argument('path', metavar='FILE')
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
     return parser
 
 
