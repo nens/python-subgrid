695845b68608417e54228eb9ac0dd56e3cc568dd
Fedor Baart <fedor.baart@deltares.nl>
2016-02-15 14:20:17 +0700

    Implement url's

--- python_subgrid/tests/performance/server.py
+++ python_subgrid/tests/performance/server.py
@@ -4,55 +4,67 @@ import threading
 import time
 import json
 import datetime
+import itertools
 
 import tornado.web
 import tornado.websocket
-import six
 
-logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+
 class WebSocket(tornado.websocket.WebSocketHandler):
     def __init__(self, application, request, **kwargs):
-        # self.zmqstream = kwargs.pop('zmqstream')
         tornado.websocket.WebSocketHandler.__init__(
             self, application, request, **kwargs)
+        self.url = None
 
-    def initialize(self):
-        pass
+    def initialize(self, url):
+        self.url = url
 
     def open(self):
         logger.debug("websocket opened ")
-        def sender():
-            for i in range(100):
-                time.sleep(5)
-                msg = {"now": datetime.datetime.now().isoformat()}
-                self.write_message(json.dumps(msg))
-        self.thread = threading.Thread(target=sender)
-        self.thread.start()
+        if (self.url == 'publish'):
+            def sender():
+                # just keep on looping
+                counter = itertools.counter()
+                for i in counter:
+                    time.sleep(1)
+                    msg = {
+                        "now": datetime.datetime.now().isoformat(),
+                        "i": i
+                    }
+                    self.write_message(json.dumps(msg))
+            self.thread = threading.Thread(target=sender)
+            self.thread.start()
     def on_message(self, message):
         # unicode, metadata message
-        logger.debug("got message %20s, text: %s", message, isinstance(message, six.text_type))
+        self.write_message(message)
 
     def on_close(self):
         logger.debug("websocket closed")
 
 
 class HTTPHandler(tornado.web.RequestHandler):
-    def initialize(self):
-        pass
+    def initialize(self, size):
+        # Message of 1MB
+        message = '1' * 1000 * 1000
+        self.message = size * message
 
     def get(self):
-        self.set_header("Content-Type", "application/json")
-        self.write('ok')
+        self.set_header("Content-Type", "application/octet-stream")
+        self.write(self.message)
 
 def app():
     # register socket
     application = tornado.web.Application([
-        (r"/", HTTPHandler),
+        (r"/echo", WebSocket, dict(url='echo')),
+        (r"/publish", WebSocket, dict(url='publish')),
         # todo use an id scheme to attach to multiple models
-        (r"/ws", WebSocket),
+        (r"/empty", HTTPHandler, dict(size=0)),
+        (r"/100MB", HTTPHandler, dict(size=100))
     ])
     return application
 
